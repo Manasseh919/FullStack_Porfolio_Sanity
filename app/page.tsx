9cd92f816acc75a6edfa30dd5599f121aa078825
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-06-05 10:36:26 +0000

    initialized project

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+            {/* trying */}
             By{' '}
             <Image
               src="/vercel.svg"
